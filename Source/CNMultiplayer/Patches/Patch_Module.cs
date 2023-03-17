b4eff976304957db4e430bb198f7d1aab1529f14
winnie1069667084 <1069667084@qq.com>
2023-03-17 15:00:41 +0800

    提升XML的加载优先级，重构XML文件

---
+++ Source/CNMultiplayer/Patches/Patch_Module.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using TaleWorlds.ObjectSystem;
+
+namespace Patches
+{
+    [HarmonyPatch(typeof(TaleWorlds.MountAndBlade.Module), "LoadSubModules")]//优先读取CNM的xml文件
+    internal class Patch_LoadSubModules
+    {
+        public static bool Prefix()
+        {
+            XmlResource.GetXmlListAndApply("CNMultiplayer");
+            return true;
+        }
+    }
+}
