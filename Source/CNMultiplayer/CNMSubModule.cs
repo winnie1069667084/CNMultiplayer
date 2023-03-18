84a7f56f8146500c7a4f0fa8345d8f935f23232c
winnie1069667084 <1069667084@qq.com>
2023-03-18 13:14:07 +0800

    1.重写xml加载逻辑，分离各个模式XML文件 2.第三perk皮肤系统测试

--- Source/CNMultiplayer/CNMSubModule.cs
+++ Source/CNMultiplayer/CNMSubModule.cs
@@ -1,5 +1,10 @@
 ﻿using HarmonyLib;
+using System.Xml;
+using TaleWorlds.CampaignSystem.ViewModelCollection.ClanManagement;
+using TaleWorlds.Core;
+using TaleWorlds.ModuleManager;
 using TaleWorlds.MountAndBlade;
+using TaleWorlds.ObjectSystem;
 
 namespace CNMultiplayer
 {
@@ -11,6 +16,12 @@ namespace CNMultiplayer
             Harmony harmony = new Harmony("CNMultiplayer");
             harmony.PatchAll();
             //待学习调整：*本地化路径修改、*地图投票界面、WelcomeMessage、领军超8人炸服
+        }
+
+        public override void OnBeforeMissionBehaviorInitialize(Mission mission)
+        {
+            var ld = new LoadXMLbyMode();
+            ld.ModeJudgment();
         }
     }
 }
