ed1d599f45ae4b1c0558b8a9cf63d88626cc689d
winnie1069667084 <1069667084@qq.com>
2023-03-30 11:43:11 +0800

    修复因mpitems.xslt导致进入服务器时的崩溃（恢复使用Item|CraftedItem）

--- Source/CNMultiplayer/CNMSubModule.cs
+++ Source/CNMultiplayer/CNMSubModule.cs
@@ -1,8 +1,5 @@
 ﻿using HarmonyLib;
-using System.Xml;
-using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
-using TaleWorlds.ObjectSystem;
 
 namespace CNMultiplayer
 {
@@ -13,7 +10,7 @@ namespace CNMultiplayer
             base.OnSubModuleLoad();
             Harmony harmony = new Harmony("CNMultiplayer");
             harmony.PatchAll();
-            //待学习调整：*本地化路径修改、*地图投票界面、WelcomeMessage、领军超8人炸服、不同模式加载不同XML
+            //待学习调整：*地图投票界面、WelcomeMessage、*不同模式加载不同XML
         }
     }
 }
