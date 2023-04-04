76b9589103948a57afb522125ff1ff5a74171937
winnie1069667084 <1069667084@qq.com>
2023-04-05 06:28:51 +0800

    移除攻城模式的骑兵AI

---
+++ Source/CNMultiplayer_ServerOnly/Patches/Patch_SpawningBehaviorBase.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace Patches
+{
+    [HarmonyPatch(typeof(SpawningBehaviorBase), "SpawnBot")]//修改Bot重生规则（仅限攻城模式）
+    internal class Patch_SpawnBot
+    {
+        public static bool Prefix(Team agentTeam, BasicCultureObject cultureLimit, SpawnComponent ___SpawnComponent, MissionLobbyComponent ___MissionLobbyComponent)
+        {
+            if (MultiplayerOptions.OptionType.GameType.GetStrValue(MultiplayerOptions.MultiplayerOptionsAccessMode.CurrentMapOptions) != "Siege")
+            { return true; }
+            BasicCharacterObject troopCharacter = MultiplayerClassDivisions.GetMPHeroClasses(cultureLimit).ToMBList<MultiplayerClassDivisions.MPHeroClass>().GetRandomElement<MultiplayerClassDivisions.MPHeroClass>()
+    .TroopCharacter;
+            if (troopCharacter.IsMounted)//删除骑兵AI
+            {
+                return false;
+            }
+            MatrixFrame spawnFrame = ___SpawnComponent.GetSpawnFrame(agentTeam, troopCharacter.HasMount(), true);
+            AgentBuildData agentBuildData = new AgentBuildData(troopCharacter).Team(agentTeam).InitialPosition(spawnFrame.origin);
+            Vec2 vec = spawnFrame.rotation.f.AsVec2;
+            vec = vec.Normalized();
+            AgentBuildData agentBuildData2 = agentBuildData.InitialDirection(vec).TroopOrigin(new BasicBattleAgentOrigin(troopCharacter)).EquipmentSeed(___MissionLobbyComponent.GetRandomFaceSeedForCharacter(troopCharacter, 0))
+                .ClothingColor1((agentTeam.Side == BattleSideEnum.Attacker) ? cultureLimit.Color : cultureLimit.ClothAlternativeColor)
+                .ClothingColor2((agentTeam.Side == BattleSideEnum.Attacker) ? cultureLimit.Color2 : cultureLimit.ClothAlternativeColor2)
+                .IsFemale(troopCharacter.IsFemale);
+            agentBuildData2.Equipment(Equipment.GetRandomEquipmentElements(troopCharacter, !(Game.Current.GameType is MultiplayerGame), false, agentBuildData2.AgentEquipmentSeed));
+            agentBuildData2.BodyProperties(BodyProperties.GetRandomBodyProperties(agentBuildData2.AgentRace, agentBuildData2.AgentIsFemale, troopCharacter.GetBodyPropertiesMin(false), troopCharacter.GetBodyPropertiesMax(), (int)agentBuildData2.AgentOverridenSpawnEquipment.HairCoverType, agentBuildData2.AgentEquipmentSeed, troopCharacter.HairTags, troopCharacter.BeardTags, troopCharacter.TattooTags));
+            Mission.Current.SpawnAgent(agentBuildData2, false).AIStateFlags |= Agent.AIStateFlag.Alarmed;
+            return false;
+        }
+    }
+}
