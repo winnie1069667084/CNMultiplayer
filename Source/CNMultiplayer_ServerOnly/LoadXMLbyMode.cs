0eaad50271c7de9d7b168c5d2956d0a4da62b2d2
winnie1069667084 <1069667084@qq.com>
2023-04-07 22:30:15 +0800

    使用旧的LoadXMLbyModes精简xml数量

--- Source/CNMultiplayer_ServerOnly/LoadXMLbyMode.cs
+++ Source/CNMultiplayer_ServerOnly/LoadXMLbyMode.cs
@@ -1,12 +1,8 @@
-﻿using System.Collections.Generic;
-using System;
-using System.IO;
+﻿using System.IO;
 using System.Xml;
 using TaleWorlds.ModuleManager;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.ObjectSystem;
-using TaleWorlds.Library;
-using TaleWorlds.Core;
 
 namespace CNMultiplayer
 {
@@ -14,121 +10,40 @@ namespace CNMultiplayer
     {
         public void ModeJudgment()
         {
-            var List = new List<string>() { "MPCharacters", "Items", "MPClassDivisions", "Monsters" };
-            bool isDevelopment = Game.Current.GameType.IsDevelopment;
-            string gameType = Game.Current.GameType.GetType().Name;
             string gametype = MultiplayerOptions.OptionType.GameType.GetStrValue(MultiplayerOptions.MultiplayerOptionsAccessMode.CurrentMapOptions);
             switch (gametype)
             {
                 case "Siege":
-                    foreach (string id in List)
-                    {
-                        XmlDocument mergedXmlForManaged = LoadXMLbyMode.GetMergedXmlForManaged(id, false, gametype, isDevelopment, gameType);
-                        MBObjectManager.Instance.LoadXml(mergedXmlForManaged, isDevelopment);
-                    }
+                    LoadXMLs("CNMultiplayer", "Siege");
                     break;
                 case "Captain":
-                    foreach (string id in List)
-                    {
-                        XmlDocument mergedXmlForManaged = LoadXMLbyMode.GetMergedXmlForManaged(id, false, gametype, isDevelopment, gameType);
-                        MBObjectManager.Instance.LoadXml(mergedXmlForManaged, isDevelopment);
-                    }
+                    LoadXMLs("CNMultiplayer", "Captain");
                     break;
                 default:
-                    foreach (string id in List)
-                    {
-                        XmlDocument mergedXmlForManaged = LoadXMLbyMode.GetMergedXmlForManaged(id, false, "", isDevelopment, gameType);
-                        MBObjectManager.Instance.LoadXml(mergedXmlForManaged, isDevelopment);
-                    }
+                    LoadXMLs("Native", "");
                     break;
             }
         }
 
-        public static XmlDocument GetMergedXmlForManaged(string id, bool skipValidation, string modename, bool ignoreGameTypeInclusionCheck = true, string gameType = "")
+        public void LoadXMLs(string modulename, string modename)
         {
-            List<Tuple<string, string>> list = new List<Tuple<string, string>>();
-            List<string> list2 = new List<string>();
-            foreach (MbObjectXmlInformation mbObjectXmlInformation in XmlResource.XmlInformationList)
+            string path = ModuleHelper.GetModuleFullPath(modulename) + modename + "ModuleData/";
+            DirectoryInfo d = new DirectoryInfo(path);
+            foreach (string filename in filelist)
             {
-                if (mbObjectXmlInformation.Id == id && (ignoreGameTypeInclusionCheck || mbObjectXmlInformation.GameTypesIncluded.Count == 0 || mbObjectXmlInformation.GameTypesIncluded.Contains(gameType)))
+                FileInfo[] files = d.GetFiles(filename);
+                foreach (FileInfo file in files)
                 {
-                    string xsdPath = ModuleHelper.GetXsdPath(mbObjectXmlInformation.Id);
-                    string text = ModuleHelper.GetXmlPath(mbObjectXmlInformation.ModuleName, mbObjectXmlInformation.Name);
-                    if (mbObjectXmlInformation.ModuleName == "CNMultiplayer")
-                    {
-                        text = GetXmlPath(mbObjectXmlInformation.ModuleName, mbObjectXmlInformation.Name, modename);
-                    }
-                    if (File.Exists(text))
-                    {
-                        if (mbObjectXmlInformation.ModuleName == "CNMultiplayer")
-                        {
-                            list.Add(Tuple.Create<string, string>(GetXmlPath(mbObjectXmlInformation.ModuleName, mbObjectXmlInformation.Name, modename), xsdPath));
-                            HandleXsltList(GetXsltPath(mbObjectXmlInformation.ModuleName, mbObjectXmlInformation.Name, modename), ref list2);
-                        }
-                        else
-                        {
-                            list.Add(Tuple.Create<string, string>(ModuleHelper.GetXmlPath(mbObjectXmlInformation.ModuleName, mbObjectXmlInformation.Name), xsdPath));
-                            HandleXsltList(ModuleHelper.GetXsltPath(mbObjectXmlInformation.ModuleName, mbObjectXmlInformation.Name), ref list2);
-                        }
-                    }
-                    else
-                    {
-                        string text2 = text.Replace(".xml", "");
-                        if (Directory.Exists(text2))
-                        {
-                            foreach (FileInfo fileInfo in new DirectoryInfo(text2).GetFiles("*.xml"))
-                            {
-                                text = text2 + "/" + fileInfo.Name;
-                                list.Add(Tuple.Create<string, string>(text, xsdPath));
-                                HandleXsltList(text.Replace(".xml", ".xsl"), ref list2);
-                            }
-                        }
-                        else
-                        {
-                            list.Add(Tuple.Create<string, string>("", ""));
-                            if (!HandleXsltList(ModuleHelper.GetXsltPath(mbObjectXmlInformation.ModuleName, mbObjectXmlInformation.Name), ref list2))
-                            {
-                                Debug.ShowError(string.Concat(new string[]
-                                {
-                                    "Unable to find xml or xslt file for the entry '",
-                                    ModuleHelper.GetModuleFullPath(mbObjectXmlInformation.ModuleName),
-                                    "ModuleData/",
-                                    mbObjectXmlInformation.Name,
-                                    "' in SubModule.xml."
-                                }));
-                            }
-                        }
-                    }
+                    XmlDocument xmlDocument = new XmlDocument();
+                    xmlDocument.Load(file.FullName);
+                    MBObjectManager.Instance.LoadXml(xmlDocument);
                 }
             }
-            return MBObjectManager.CreateMergedXmlFile(list, list2, skipValidation);
         }
 
-        private static bool HandleXsltList(string xslPath, ref List<string> xsltList)
+        string[] filelist = new string[]
         {
-            string text = xslPath + "t";
-            if (File.Exists(xslPath))
-            {
-                xsltList.Add(xslPath);
-                return true;
-            }
-            if (File.Exists(text))
-            {
-                xsltList.Add(text);
-                return true;
-            }
-            xsltList.Add("");
-            return false;
-        }
-
-        public static string GetXmlPath(string moduleId, string xmlName, string modename)
-        {
-            return ModuleHelper.GetModuleFullPath(moduleId) + modename + "ModuleData/" + xmlName + ".xml";
-        }
-
-        public static string GetXsltPath(string moduleId, string xmlName, string modename)
-        {
-            return ModuleHelper.GetModuleFullPath(moduleId) + modename + "ModuleData/" + xmlName + ".xsl";
-        }
+            "mpclassdivisions.xml",
+        };
     }
 }
