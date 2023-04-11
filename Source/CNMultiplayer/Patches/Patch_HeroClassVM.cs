ee8d4c5e63edd5b377f9ada31865c0d080724921
winnie1069667084 <1069667084@qq.com>
2023-04-11 13:02:56 +0800

    *重要功能：根据实时战场人数锁定双方的兵种比例（除步兵外的兵种，分别不得占该方总人数的1/4）。玩家视角的表现形式为金钱足够但不能选择该兵种。

---
+++ Source/CNMultiplayer/Patches/Patch_HeroClassVM.cs
@@ -0,0 +1,59 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.ViewModelCollection.Multiplayer.ClassLoadout;
+using HarmonyLib;
+
+namespace Patches
+{
+    [HarmonyPatch(typeof(HeroClassVM), "UpdateEnabled")]//根据战场兵种比例锁定兵种，限定射手、骑兵、骑射手分别不超过总兵力的1/4。
+    internal class Patch_UpdateEnabled
+    {
+        public static bool Prefix(HeroClassVM __instance, MissionMultiplayerGameModeBaseClient ____gameMode)
+        {
+            if (MultiplayerOptions.OptionType.GameType.GetStrValue(MultiplayerOptions.MultiplayerOptionsAccessMode.CurrentMapOptions) != "Siege" || !GameNetwork.IsClient)
+            { return true; }
+            bool flag = true;
+            MissionPeer component = GameNetwork.MyPeer.GetComponent<MissionPeer>();
+            int Sum = GetTroopTypeCountForTeam(component.Team)[0];
+            int Infantry = GetTroopTypeCountForTeam(component.Team)[1];
+            int Ranged = GetTroopTypeCountForTeam(component.Team)[2];
+            int Cavalry = GetTroopTypeCountForTeam(component.Team)[3];
+            int HorseArcher = GetTroopTypeCountForTeam(component.Team)[4];
+            string Id = __instance.TroopTypeId;
+            if ((Id == "Ranged" && Ranged > Sum / 4) || (Id == "Cavalry" && Cavalry > Sum / 4) || (Id == "HorseArcher" && HorseArcher > Sum / 4))
+            {
+                flag = false;
+            }
+            __instance.IsEnabled = ____gameMode.IsInWarmup || !____gameMode.IsGameModeUsingGold || (____gameMode.GetGoldAmount() >= __instance.Gold && flag);
+            return false;
+        }
+
+        public static int[] GetTroopTypeCountForTeam(Team team)//统计某方战场存活兵种数，0总数；1步兵；2射手；3骑兵；4骑射
+        {
+            int[] num = new int[5];
+            foreach (NetworkCommunicator networkPeer in GameNetwork.NetworkPeers)
+            {
+                MissionPeer component = networkPeer.GetComponent<MissionPeer>();
+                if (component?.Team != null && component.Team == team && component.IsControlledAgentActive)
+                {
+                    BasicCharacterObject Character = MultiplayerClassDivisions.GetMPHeroClassForPeer(component).HeroCharacter;
+                    num[0]++;   
+                    if (Character.IsInfantry)
+                    {
+                        num[1]++;
+                        continue;
+                    }
+                    if (Character.IsRanged)
+                        num[2]++;
+                    if (Character.IsMounted)
+                        num[3]++;
+                    if (Character.IsRanged && Character.IsMounted)
+                        num[4]++;
+                }
+            }
+            num[2] -= num[4];
+            num[3] -= num[4];
+            return num;
+        }
+    }
+}
