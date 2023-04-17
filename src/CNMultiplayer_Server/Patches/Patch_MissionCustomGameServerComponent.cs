8d55d6cdbcbca049cc0bbe80076a83a29e2f4045
winnie1069667084 <1069667084@qq.com>
2023-04-17 09:48:15 +0800

    · 一个新的Patch，Siege和TDM EndMission后不再向大厅汇报玩家战局情况（可能缓解每局换图的ShutDown）

--- src/CNMultiplayer_Server/Patches/Patch_MissionCustomGameServerComponent.cs
+++ src/CNMultiplayer_Server/Patches/Patch_MissionCustomGameServerComponent.cs
@@ -5,7 +5,7 @@ using TaleWorlds.MountAndBlade.DedicatedCustomServer;
 
 namespace Patches
 {
-    [HarmonyPatch(typeof(MissionCustomGameServerComponent), "HandleLateNewClientAfterSynchronized")]//玩家加入服务器提示
+    [HarmonyPatch(typeof(MissionCustomGameServerComponent), "HandleLateNewClientAfterSynchronized")] //玩家加入服务器提示
     internal class Patch_HandleLateNewClientAfterSynchronized
     {
         public static void Postfix(NetworkCommunicator networkPeer)
@@ -13,6 +13,15 @@ namespace Patches
             GameNetwork.BeginBroadcastModuleEvent();
             GameNetwork.WriteMessage(new ServerMessage("[服务器]: " + networkPeer.UserName.ToString() + " 加入游戏", false));
             GameNetwork.EndBroadcastModuleEvent(GameNetwork.EventBroadcastFlags.None, null);
+        }
+    }
+
+    [HarmonyPatch(typeof(MissionCustomGameServerComponent), "AddScoresToStats")] //End_Mission后不统计大厅分数
+    internal class Patch_AddScoresToStats
+    {
+        public static bool Prefix()
+        {
+            return false;
         }
     }
 }
