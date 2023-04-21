8bb3b00f921429a2e78ac54d4307b93b4799aeee
winnie1069667084 <1069667084@qq.com>
2023-04-21 09:56:46 +0800

    · CNMCaptainGameMode

---
+++ src/CNMultiplayer_Server/Modes/Captain/CNMCaptainGameMode.cs
@@ -0,0 +1,117 @@
+﻿using CNMultiplayer.Modes.Warmup;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.Source.Missions;
+
+#if CLIENT
+using TaleWorlds.MountAndBlade.View.MissionViews;
+using TaleWorlds.MountAndBlade.View;
+#endif
+
+namespace CNMultiplayer.Modes.Captain
+{
+#if CLIENT
+    [ViewCreatorModule] // Exposes methods with ViewMethod attribute.
+#endif
+    internal class CNMCaptainGameMode : MissionBasedMultiplayerGameMode
+    {
+        private const string GameName = "CNMCaptain";
+
+        public CNMCaptainGameMode()
+            : base(GameName)
+        { }
+#if CLIENT
+        [ViewMethod(GameName)]
+        public static MissionView[] OpenCNMSiege(Mission mission)
+        {
+            return new[]
+            {
+                ViewCreator.CreateMissionServerStatusUIHandler(),
+                ViewCreator.CreateMultiplayerFactionBanVoteUIHandler(), // None Native
+                ViewCreator.CreateMissionMultiplayerPreloadView(mission),
+                ViewCreator.CreateMissionKillNotificationUIHandler(),
+                ViewCreator.CreateMissionAgentStatusUIHandler(mission),
+                ViewCreator.CreateMissionMainAgentEquipmentController(mission),
+                ViewCreator.CreateMissionMainAgentCheerBarkControllerView(mission),
+                ViewCreator.CreateMissionMultiplayerEscapeMenu("Siege"),
+                ViewCreator.CreateMultiplayerEndOfBattleUIHandler(),
+                ViewCreator.CreateMissionAgentLabelUIHandler(mission),
+                ViewCreator.CreateMultiplayerTeamSelectUIHandler(),
+                ViewCreator.CreateMissionScoreBoardUIHandler(mission, isSingleTeam: false),
+                ViewCreator.CreateMultiplayerEndOfRoundUIHandler(),
+                ViewCreator.CreateLobbyEquipmentUIHandler(),
+                ViewCreator.CreatePollProgressUIHandler(),
+                ViewCreator.CreateMultiplayerMissionHUDExtensionUIHandler(),
+                ViewCreator.CreateMultiplayerMissionDeathCardUIHandler(),
+                ViewCreator.CreateMissionFlagMarkerUIHandler(), // Draw flags when pressing ALT.
+                ViewCreator.CreateOptionsUIHandler(),
+                ViewCreator.CreateMissionMainAgentEquipDropView(mission),
+                ViewCreator.CreateMissionBoundaryCrossingView(),
+                new MissionItemContourControllerView(), // Draw contour of item on the ground when pressing ALT.
+                new MissionAgentContourControllerView(),
+                new MissionBoundaryWallView(),
+                new SpectatorCameraView(), // None Native
+            };
+        }
+#endif
+
+        public override void StartMultiplayerGame(string scene)
+        {
+            MissionState.OpenNew(GameName, new MissionInitializerRecord(scene)
+            { SceneUpgradeLevel = 3, SceneLevels = string.Empty },
+            _ => (GameNetwork.IsServer)
+            ? new MissionBehavior[] // Server side behavior
+            {
+                MissionLobbyComponent.CreateBehavior(),
+                new MissionMultiplayerFlagDomination(MissionLobbyComponent.MultiplayerGameType.Captain),
+                new MultiplayerRoundController(),
+                new SpawnComponent(new FlagDominationSpawnFrameBehavior(), new FlagDominationSpawningBehavior()),
+                new CNMWarmupComponent(() => (new FlagDominationSpawnFrameBehavior(), new FlagDominationSpawningBehavior())),
+                new MissionMultiplayerGameModeFlagDominationClient(),
+                new MultiplayerTimerComponent(),
+                new MultiplayerMissionAgentVisualSpawnComponent(),
+                new ConsoleMatchStartEndHandler(),
+                new MissionLobbyEquipmentNetworkComponent(),
+                new MultiplayerTeamSelectComponent(),
+                new MissionHardBorderPlacer(),
+                new MissionBoundaryPlacer(),
+                new MissionBoundaryCrossingHandler(),
+                new MultiplayerPollComponent(),
+                new MultiplayerAdminComponent(),
+                new MultiplayerGameNotificationsComponent(),
+                new MissionOptionsComponent(),
+                new MissionScoreboardComponent(new CaptainScoreboardData()),
+                new MissionAgentPanicHandler(),
+                new AgentVictoryLogic(),
+                new AgentHumanAILogic(),
+                new EquipmentControllerLeaveLogic(),
+                new MultiplayerPreloadHelper()
+            }
+
+            : new MissionBehavior[] // Client side behavior
+            {
+                MissionLobbyComponent.CreateBehavior(),
+                new CNMWarmupComponent(() => (new FlagDominationSpawnFrameBehavior(), new FlagDominationSpawningBehavior())),
+                new MissionMultiplayerGameModeFlagDominationClient(),
+                new MultiplayerAchievementComponent(),
+                new MultiplayerTimerComponent(),
+                new MultiplayerRoundComponent(),
+                new MultiplayerMissionAgentVisualSpawnComponent(),
+                new ConsoleMatchStartEndHandler(),
+                new MissionLobbyEquipmentNetworkComponent(),
+                new MultiplayerTeamSelectComponent(),
+                new MissionHardBorderPlacer(),
+                new MissionBoundaryPlacer(),
+                new MissionBoundaryCrossingHandler(),
+                new MultiplayerPollComponent(),
+                new MultiplayerGameNotificationsComponent(),
+                new MissionOptionsComponent(),
+                new MissionScoreboardComponent(new CaptainScoreboardData()),
+                new MissionMatchHistoryComponent(),
+                new EquipmentControllerLeaveLogic(),
+                new MissionRecentPlayersComponent(),
+                new MultiplayerPreloadHelper()
+            });
+        }
+    }
+}
