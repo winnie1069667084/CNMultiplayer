085cf9468d4cc3c6ca6fed35565eed60bc07e448
winnie1069667084 <1069667084@qq.com>
2023-04-23 23:46:29 +0800

    · 给服务器添加了若干Patch，尝试修复换图崩溃

---
+++ src/CNMultiplayer_Server/Patches/Patch_CustomBattleServer.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.Diamond;
+
+namespace CNMultiplayer.Patches
+{
+    [HarmonyPatch(typeof(CustomBattleServer), "UpdateBattleStats")]
+    internal class Patch_UpdateBattleStats
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(CustomBattleServer), "BattleFinished")]
+    internal class Patch_BattleFinished
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(CustomBattleServer), "BattleStarted")]
+    internal class Patch_BattleStarted
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(CustomBattleServer), "BeforeStartingNextBattle")]
+    internal class Patch_BeforeStartingNextBattle
+    {
+        public static bool Prefix(IBadgeComponent ____badgeComponent)
+        {
+            IBadgeComponent badgeComponent = ____badgeComponent;
+            badgeComponent?.OnStartingNextBattle();
+            return false;
+        }
+    }
+}
