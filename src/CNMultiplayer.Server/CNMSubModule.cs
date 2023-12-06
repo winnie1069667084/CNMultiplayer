0ce4936bb5c4530255736fb7f1cff6bd36f73597
winnie1069667084 <1069667084@qq.com>
2023-12-07 05:55:13 +0800

    · 重写csproj · 更改项目架构（Common待完成）

---
+++ src/CNMultiplayer.Server/CNMSubModule.cs
@@ -0,0 +1,24 @@
+﻿using CNMultiplayer.Server.Modes.CNMSiege;
+using CNMultiplayer.Server.Patches.Behaviors;
+using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace CNMultiplayer.Server
+{
+    public class CNMSubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Harmony harmony = new Harmony("CNMultiplayer");
+            harmony.PatchAll();
+            Module.CurrentModule.AddMultiplayerGameMode(new CNMSiegeGameMode());
+        }
+
+        public override void OnBeforeMissionBehaviorInitialize(Mission mission)
+        {
+            // 修复“有玩家未准备好”bug，by mentalrob
+            mission.AddMissionBehavior(new NotAllPlayersJoinFixBehavior());
+        }
+    }
+}
